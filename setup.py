43d235596a55e1f70e23d237a8c1b3b229d05b28
Bintang A.S.W.A.M <23573683+baswam95@users.noreply.github.com>
2022-12-22 14:14:43 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name="HPPL final project",
+      author="Lyubits* Flow",
+      description = 
+      '''
+      Numerical and Physical modeling of Pressure-Driven Pipe Flow, Hagen-Poiseuille Flow
+      ''',
+      install_requires = [
+          "numpy >= 1.21.6"
+          "cupy  >= 11.0.0"
+          "numba >= 0.56.4"
+          "tqdm  >= 4.64.1"
+          "matplotlib >= 3.2.2"
+      ],
+
+long_description = open('README.md').read()
+)
